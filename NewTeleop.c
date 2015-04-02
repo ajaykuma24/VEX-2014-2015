16ef2fbc655f766d6247366e01419343968ab6b8
ajaykuma24 <ajaykuma24@yahoo.com>
2015-04-02 18:09:27 -0400

    Changed controls (again)
    Arms on 1 joystick & individual buttons. Both controllers can control
    intake.

--- NewTeleop.c
+++ NewTeleop.c
@@ -16,62 +16,76 @@ task main()
 	while(1)
 	{
 		if(abs(vexRT[Ch3])>thresh)
-				{
-						motor[FLeft] = vexRT[Ch3];
-						motor[RLeft] =  vexRT[Ch3];
-				}
-				else if(abs(vexRT[Ch3])<=thresh)
-				{
-						motor[FLeft] = 0;
-						motor[RLeft] =  0;
-				}
-				if(abs(vexRT[Ch2])>thresh)
-				{
-						motor[FRight] = vexRT[Ch2];
-						motor[RRight] =  vexRT[Ch2];
-				}
-				else if(abs(vexRT[Ch2])<=thresh)
-				{
-						motor[FRight] = 0;
-						motor[RRight] =  0;
-				}
+		{
+				motor[FLeft] = vexRT[Ch3];
+				motor[RLeft] =  vexRT[Ch3];
+		}
+		else if(abs(vexRT[Ch3])<=thresh)
+		{
+				motor[FLeft] = 0;
+				motor[RLeft] =  0;
+		}
+		if(abs(vexRT[Ch2])>thresh)
+		{
+				motor[FRight] = vexRT[Ch2];
+				motor[RRight] =  vexRT[Ch2];
+		}
+		else if(abs(vexRT[Ch2])<=thresh)
+		{
+				motor[FRight] = 0;
+				motor[RRight] =  0;
+		}
+		if(abs(vexRT[Ch2Xmtr2])>thresh&&(!vexRT[Btn5DXmtr2]&&!vexRT[Btn6DXmtr2]&&!vexRT[Btn5UXmtr2]&&!vexRT[Btn6UXmtr2]))
+		{
+				motor[TLeft] =  vexRT[Ch2Xmtr2];
+				motor[BLeft] =  vexRT[Ch2Xmtr2];
+				motor[TRight] =  vexRT[Ch2Xmtr2];
+				motor[BRight] =  vexRT[Ch2Xmtr2];
+		}
 
-		if(abs(vexRT[Ch2Xmtr2])>thresh)
-				{
-						motor[TLeft] =  vexRT[Ch2Xmtr2];
-						motor[BLeft] =  vexRT[Ch2Xmtr2];
-				}
-				else if(abs(vexRT[Ch2Xmtr2])<=thresh)
-				{
-						motor[TLeft] = 0;
-						motor[BLeft] = 0;
-				}
+		else if(vexRT[Btn6UXmtr2]&&abs(vexRT[Ch2Xmtr2])<thresh)
+		{
+				motor[TLeft] =  70;
+				motor[BLeft] =  70;
+		}
+		else if(vexRT[Btn6DXmtr2]&&abs(vexRT[Ch2Xmtr2])<thresh)
+		{
+				motor[TLeft] =  -70;
+				motor[BLeft] =  -70;
+		}
+		else if(vexRT[Btn5UXmtr2]&&abs(vexRT[Ch2Xmtr2])<thresh)
+		{
+				motor[TRight] =  70;
+				motor[BRight] =  70;
+		}
+		else if(vexRT[Btn5DXmtr2]&&abs(vexRT[Ch2Xmtr2])<thresh)
+		{
+				motor[TRight] =  -70;
+				motor[BRight] =  -70;
+		}
+		else if(abs(vexRT[Ch2Xmtr2])<=thresh&&(!vexRT[Btn5DXmtr2]&&!vexRT[Btn6DXmtr2]&&!vexRT[Btn5UXmtr2]&&!vexRT[Btn6UXmtr2]))
+		{
+				motor[TLeft] = 0;
+				motor[BLeft] = 0;
+				motor[TRight] = 0;
+				motor[BRight] = 0;
+		}
 
-		if(abs(vexRT[Ch3Xmtr2])>thresh)
-				{
-						motor[TRight] =  vexRT[Ch3Xmtr2];
-						motor[BRight] =  vexRT[Ch3Xmtr2];
-				}
-				else if(abs(vexRT[Ch3Xmtr2])<=thresh)
-				{		motor[TRight] = 0;
-						motor[BRight] = 0;
-				}
-
-		if(vexRT[Btn6U])
-				{
-						motor[RIntake] = 100;
-						motor[LIntake] =  100;
-				}
-
-				else if(vexRT[Btn6D])
-				{
-						motor[RIntake] = -100;
-						motor[LIntake] = -100;
-				}
-				else
-				{
-						motor[RIntake] = 0;
-						motor[LIntake] = 0;
-				}
+		if(vexRT[Btn6U]||(vexRT[Ch3Xmtr2]>thresh&&!vexRT[Btn6U]&&!vexRT[Btn6D]))
+		{
+				motor[RIntake] = 100;
+				motor[LIntake] =  100;
+		}
+			else if(vexRT[Btn6D]||(vexRT[Ch3Xmtr2]<-thresh&&!vexRT[Btn6U]&&!vexRT[Btn6D]))
+		{
+				motor[RIntake] = -100;
+				motor[LIntake] = -100;
+		}
+		else
+		{
+				motor[RIntake] = 0;
+				motor[LIntake] = 0;
+		}
+		wait1Msec(30);
 	}
 }
